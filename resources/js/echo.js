5607b7c489d1abc3d9993e489c5e13cebec996f0
ivezagod <nemanjaivezic1@gmail.com>
2025-04-09 17:34:03 +0200

    Resposnsive design

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,8 +6,8 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsHost: 'ws.drawit.cfd',
     wsPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
